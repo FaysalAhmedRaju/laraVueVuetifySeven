2455a5fa85ff0011f8821d97127680908de0c4e1
FaysalAhmedRaju <faysalahmediiuc@gmail.com>
2020-07-06 20:08:51 +0600

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Hello Bangladesh</div>
+
+                    <div class="card-body">
+                        Hello Bangladesh!!!!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content" id="app">
+                <div class="title m-b-md">
+                   <example-component></example-component>
+                </div>
+            </div>
+        </div>
+    </body>
+    <script src="{{asset('js/app.js')}}"></script>
+</html>
